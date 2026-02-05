eeef2e8c97b3fa15d34a805e8714918663a74294
unknown <harshkumar34.fin@gmail.com>
2026-02-05 20:34:41 +0530

    bug fixes

--- server/lib/prisma.ts
+++ server/lib/prisma.ts
@@ -3,7 +3,6 @@ import { PrismaPg } from '@prisma/adapter-pg'
 import prismaPkg from "../generated/prisma/index.js"
 const { PrismaClient } = prismaPkg
 
-
 const connectionString = `${process.env.DATABASE_URL}`
 
 const adapter = new PrismaPg({ connectionString })
