19a06eefe0244c9dd94ccab8c0d876cfaddbb17c
unknown <harshkumar34.fin@gmail.com>
2026-02-05 20:53:25 +0530

    bug fix

--- server/lib/prisma.ts
+++ server/lib/prisma.ts
@@ -1,11 +1,20 @@
-import "dotenv/config";
-import { PrismaPg } from '@prisma/adapter-pg'
-import prismaPkg from "../generated/prisma/index.js"
+import "dotenv/config"
+import { PrismaPg } from "@prisma/adapter-pg"
+import { createRequire } from "module"
+
+const require = createRequire(import.meta.url)
+
+// ⬇️ THIS IS THE KEY LINE
+const prismaPkg = require("../generated/prisma/index.js")
+
 const { PrismaClient } = prismaPkg
 
-const connectionString = `${process.env.DATABASE_URL}`
+const connectionString = process.env.DATABASE_URL
+if (!connectionString) {
+  throw new Error("DATABASE_URL is not set")
+}
 
 const adapter = new PrismaPg({ connectionString })
 const prisma = new PrismaClient({ adapter })
 
-export default prisma;
+export default prisma
